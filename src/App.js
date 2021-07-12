500c31ae1ddf21e0e1f15ab8fd9044ea439e8f88
Mac Rowe <m.rowe42@gmail.com>
2021-07-12 17:09:10 -0400

    AReturn AlbumList component in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import AlbumList from './containers/AlbumList';
 
 function App() {
   
@@ -67,20 +67,7 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AlbumList aL={albums} />
     </div>
   );
 }
