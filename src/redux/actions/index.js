8f579832731b539af47fea46363bca3a5b64ac82
Mac Rowe <m.rowe42@gmail.com>
2021-07-13 13:15:34 -0400

    Fix typo for fetchAlbums to not take in argument

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,7 @@ export const fetchAlbums = () => {
     return(dispatch) => {
         fetch('http://localhost3000/albums')
             .then(response => response.json())
-            .then(albums => {
+            .then(albums => { console.log(albums)
                 dispatch({ type: 'FETCH_ALBUMS', payload: albums})
             })
     }
