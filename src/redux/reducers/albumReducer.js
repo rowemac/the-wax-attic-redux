bc36c8775de56e23aca273fcc607e4bb7d8d2386
Mac Rowe <m.rowe42@gmail.com>
2021-07-13 15:20:35 -0400

    Create ADD_ALBUM case for albumReducer

--- src/redux/reducers/albumReducer.js
+++ src/redux/reducers/albumReducer.js
@@ -3,6 +3,8 @@ export default ( state = [], action ) => {
     switch(action.type) {
         case "FETCH_ALBUMS":
             return [...action.payload]
+        case "ADD_ALBUM":
+            return [...state, action.payload]
         default:
             return state
     }
