0e04b04ff6f674315f2d4708454495c476837ce0
Mac Rowe <m.rowe42@gmail.com>
2021-07-13 14:42:59 -0400

    Update fetchAlbums address

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,7 @@
 export const fetchAlbums = () => {
 
     return(dispatch) => {
-        fetch('http://localhost3000/albums')
+        fetch('http://127.0.0.1:3000/albums')
             .then(response => response.json())
             .then(albums => { console.log(albums)
                 dispatch({ type: 'FETCH_ALBUMS', payload: albums})
