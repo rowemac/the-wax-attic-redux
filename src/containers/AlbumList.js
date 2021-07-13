303bca9513d426f822e28f165570337c9da803f8
Mac Rowe <m.rowe42@gmail.com>
2021-07-13 12:43:15 -0400

    Alter file structure and prepare AlbumList to interact with redux store

--- src/containers/AlbumList.js
+++ src/containers/AlbumList.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import Album from '../components/Album'
+import NewAlbumForm from '../components/NewAlbumForm';
 
 class AlbumList extends Component {
     
@@ -16,19 +17,23 @@ class AlbumList extends Component {
         })
     }
 
+    submitHandlerNewAlbum = (newAlbum) => {
+        this.setState({
+            albumAll: [...this.state.albumAll, newAlbum]
+        })
+    }
+
     render() {
-
-        const mappedAlbums = this.state.albumAll.map( (album, i) => { return <Album album={album} key={i} /> })
-
-        return(
+        const mappedAlbums = this.state.albumAll.map( (album, i) => { return <Album album={album} key={i} submitData={this.submitHandlerNewAlbum} /> })
+        
+        return(<>
             <div className="albums-container">
                 {mappedAlbums}
             </div>
-        )
+            <h4>Add an Album</h4>
+            <div><NewAlbumForm newAlbumProp={this.submitHandlerNewAlbum} /></div>
+        </>)
     }
-
-
-
 }
 
 export default AlbumList
