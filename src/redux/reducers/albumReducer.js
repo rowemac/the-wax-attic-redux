abb345416e0fc76273b898c402e2103925565bda
Mac Rowe <m.rowe42@gmail.com>
2021-07-14 13:48:08 -0400

    Fix type on REMOVE_ALBUM case statement

--- src/redux/reducers/albumReducer.js
+++ src/redux/reducers/albumReducer.js
@@ -1,11 +1,11 @@
+// eslint-disable-next-line
 export default ( state = [], action ) => {
-
     switch(action.type) {
         case "FETCH_ALBUMS":
             return [...action.payload]
         case "ADD_ALBUM":
             return [...state, action.payload]
-        case "REMOVE ALBUM":
+        case "REMOVE_ALBUM":
             let updatedAlbums = state.filter(album => album.id !== action.payload)
             return [...updatedAlbums]
         default:
