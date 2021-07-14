a019e6285b64813f871552da44428d5081aef4b6
Mac Rowe <m.rowe42@gmail.com>
2021-07-14 13:38:40 -0400

    Import BrowserRouter and add approporiate routess and switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import NewAlbumForm from './components/NewAlbumForm';
 import AlbumList from './containers/AlbumList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  
-  return (
-    <div className="App">
-      <AlbumList />
-      <NewAlbumForm />
-    </div>
-  );
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ AlbumList } />
+          </Switch>
+          <NewAlbumForm />
+        </Router>
+      </div>
+    );
 }
 
 export default App;
