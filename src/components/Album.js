9d3e40f078b54567cb254ea034a6023051b4f8d3
Mac Rowe <m.rowe42@gmail.com>
2021-07-15 15:51:45 -0400

    Fix NavLink path for edit button

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { removeAlbum } from '../redux/actions/index'
+import { editAlbum } from '../redux/actions/index'
 
-const Album = ({ album, removeAlbum }) => {
+const Album = ({ album, removeAlbum, editAlbum }) => {
     return (
         <div className="album-card">
             <img src={album.image} alt={album.title} />
             <p className="album-title">{album.title}</p>
             <p className="album-artist">{album.artist}</p>
-            <button onClick={ () => removeAlbum(album.id) }>Delete Album</button>
+            <NavLink to={`/albums/${album.id}/edit`}><button onClick={()=>editAlbum(album)}>Edit</button></NavLink>
+            <button onClick={ () => removeAlbum(album.id) }>Delete</button>
         </div>
     )
 }
-export default connect(null, { removeAlbum })(Album)
+export default connect(null, { removeAlbum, editAlbum })(Album)
