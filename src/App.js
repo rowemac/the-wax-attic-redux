3c68ab0250c59c2a3b731cbe039d5521cdd26eb4
Mac Rowe <m.rowe42@gmail.com>
2021-07-15 15:58:07 -0400

    Add import for EditAlbumForm, Navbar; Fix edit route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import EditAlbumForm from './components/EditAlbumForm'
 import NewAlbumForm from './components/NewAlbumForm';
 import AlbumList from './containers/AlbumList';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={ AlbumList } />
             <Route exact path="/albums/new" component={ NewAlbumForm } />
+            <Route exact path="/albums/:id/edit" component={ EditAlbumForm } />
           </Switch>
           {/* <NewAlbumForm /> */}
         </Router>
