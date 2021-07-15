b88bf720e31e5624546a275640a41f40e779e6cc
Mac Rowe <m.rowe42@gmail.com>
2021-07-15 14:17:45 -0400

    Create EditAlbumForm

--- src/components/EditAlbumForm.js
+++ src/components/EditAlbumForm.js
@@ -17,7 +17,110 @@ class EditAlbumForm extends Component {
             image: ""
         }
     }
-}
-//this.props.title???
 
+    componentDidMount(){
+        this.setState({
+            title: this.props.album.title,
+            artist: this.props.album.artist,
+            year: this.props.album.year,
+            condition: this.props.album.condition,
+            length: this.props.album.length,
+            genre: this.props.album.genre,
+            label: this.props.album.label,
+            single: this.props.album.single,
+            image: this.props.album.image
+        })
+    }
+
+    onChangeHandler = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+        this.props.addNewAlbum(this.state)
+    }
+
+    render() {
+        return(
+            <div className="new-form-div">
+                <form onSubmit={this.submitHandler}>
+                    <input type="text" 
+                        name="title" 
+                        placeholder="Title" 
+                        value={this.state.title} 
+                        onChange={this.onChangeHandler} 
+                    /><br></br>
+                    <input type="text" 
+                        name="artist" 
+                        placeholder="Artist" 
+                        value={this.state.artist} 
+                        onChange={this.onChangeHandler} 
+                    /><br></br>
+                    <input type="text" 
+                        name="year" 
+                        placeholder="Year" 
+                        value={this.state.year} 
+                        onChange={this.onChangeHandler} 
+                    /><br></br>
+                    <select
+                    name="condition"
+                    value={this.state.value} 
+                    onChange={this.onChangeHandler}
+                    defaultValue={'DEFAULT'}
+                    >
+                        <option value="DEFAULT" disabled>---Select a Conditon---</option>
+                        <option value="New">New</option>
+                        <option value="Good">Good</option>
+                        <option value="Fair">Fair</option>
+                        <option value="Poor">Poor</option>
+                    </select>
+                    <br></br>
+                    <input type="text" 
+                        name="length" 
+                        placeholder="Length" 
+                        value={this.state.length} 
+                        onChange={this.onChangeHandler} 
+                    /><br></br>
+                    <input type="text" 
+                        name="genre" 
+                        placeholder="Genre" 
+                        value={this.state.genre} 
+                        onChange={this.onChangeHandler} 
+                    /><br></br>
+                    <input type="text" 
+                        name="label" 
+                        placeholder="Label" 
+                        value={this.state.label} 
+                        onChange={this.onChangeHandler} 
+                    /><br></br>
+                    <input type="text" 
+                        name="single" 
+                        placeholder="Single" 
+                        value={this.state.single} 
+                        onChange={this.onChangeHandler} 
+                    /><br></br>
+                    <input type="text" 
+                        name="image" 
+                        placeholder="Image URL" 
+                        value={this.state.image} 
+                        onChange={this.onChangeHandler} 
+                    /><br></br>
+                    <br></br>
+                    <input type="submit" value="Add an Album" className="btn"/>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = (state, albumId) => {
+    const id = parseInt(albumId.match.params.id)
+    return {
+        album: state.find(album => album.id === id)
+    }
+}
 export default connect(mapStateToProps, { editAlbum })(EditAlbumForm)
