87fc00af658b3c366eec864b2fb7249e6ea384b5
Mac Rowe <m.rowe42@gmail.com>
2021-07-16 10:27:12 -0400

    Add search component

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Search = (props) => {
+    return (
+        <input 
+            type="text"
+            placeholder="Search albums"
+            onChange={(event) => props.callback(event.target.value)}
+        />
+    )
+}
+
+
+
+export default Search
