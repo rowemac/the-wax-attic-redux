134ea85b2a6728e3d138256398a2655c5ef2a4e9
Mac Rowe <m.rowe42@gmail.com>
2021-07-16 09:16:04 -0400

    Add /albums navlink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <NavLink to="/"></NavLink>
+            <NavLink to="/albums">Library</NavLink>
             <NavLink to="/albums/new">Add Album</NavLink>
         </div>
     )
