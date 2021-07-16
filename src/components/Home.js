4cfcebcb8573f88969e1a3265b125199d26884c0
Mac Rowe <m.rowe42@gmail.com>
2021-07-16 15:25:47 -0400

    Remove extra link on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 
 
 const Home = () => {
     return (<>
         <img className="logo" src="/wax_logo.png" alt="the wax attic." />
-        <NavLink to="/albums">Library</NavLink>
     </>)
 }
 export default Home
