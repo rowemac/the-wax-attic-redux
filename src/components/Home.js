1a95036c37a1bbc05467e6351a145fd3a5849971
Mac Rowe <m.rowe42@gmail.com>
2021-07-16 09:14:08 -0400

    Add navlink to /albums

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,9 @@ import { NavLink } from 'react-router-dom'
 
 
 const Home = () => {
-    return (
+    return (<>
         <img className="logo" src="/wax_logo.png" alt="the wax attic." />
-    )
+        <NavLink to="/albums">Library</NavLink>
+    </>)
 }
 export default Home
