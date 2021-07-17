9a6d83576760ea11adf5a777a8d90ad9091b4185
Mac Rowe <m.rowe42@gmail.com>
2021-07-17 13:02:22 -0400

    Comment out logo for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <ul className="navlinks-list">
-                {/* <li className="navlink-li"><NavLink className="navlink-logo" to="/"><img src="/Logo.png" alt="logo"/></NavLink></li> */}
+                {/* <li className="logo-navlink-li"><NavLink to="/"><img className="navlink-logo-img" src="/Logo.png" alt="logo"/></NavLink></li> */}
                 <li className="navlink-li"><NavLink className="navlink-item" to="/albums">Library</NavLink></li>
                 <li className="navlink-li"><NavLink className="navlink-item" to="/albums/new">Add Album</NavLink></li>
             </ul>
