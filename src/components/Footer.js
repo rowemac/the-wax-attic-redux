39c51775985d7b6b93a0485991314c07f4efc16e
Mac Rowe <m.rowe42@gmail.com>
2021-07-26 10:42:59 -0400

    Add logo image to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
-    return (
+    return (<>
+        <img className="logo-img" src="/Logo.png" alt="logo"></img>
         <p className="footer">© the wax attic. 2021</p>
-    )
+    </>)
 }
 
 export default Footer
