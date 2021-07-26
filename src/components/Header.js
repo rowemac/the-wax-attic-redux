e2df0a99ccdd6ba401a0eaa978c2be2a72becdfa
Mac Rowe <m.rowe42@gmail.com>
2021-07-26 10:52:27 -0400

    Make Header component a Nav Link for landing page. Add hover styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <h1 className="header">the wax attic.</h1>
+        <NavLink className="header-nav-link" to="/"><h1 className="header">the wax attic.</h1></NavLink>
     )
 }
 
